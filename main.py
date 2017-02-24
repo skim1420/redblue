d48d2734a559420500e30eec769e404a3b171b5f
Steven Kim <stevenkim@google.com>
2017-02-24 15:05:01 +0000

    content type header

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
         self.response.write('<body style=\'background-color: red\'>Hello, World!</body>')
 
 
