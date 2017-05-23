ceb369fd7fed303834b1fe255514368a86fd1568
Steven Kim <stevenkim@google.com>
2017-05-23 23:49:26 +0000

    blue

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<body style=\'background-color: red\'>Hello, World!</body>')
+        self.response.write('<body style=\'background-color: blue\'>Hello, World!</body>')
 
 
 app = webapp2.WSGIApplication([
